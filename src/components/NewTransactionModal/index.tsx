59eb1075b720b1bab23b20b058afab174a1368bd
Natally <natallyriqueto@hotmail.com>
2023-06-14 00:10:06 -0400

    add modal new transaction

---
+++ src/components/NewTransactionModal/index.tsx
@@ -0,0 +1,43 @@
+import * as Dialog from "@radix-ui/react-dialog";
+import { Overlay, Content, CloseButton, TransactionType, TransactionTypeButton } from "./styles";
+import { ArrowCircleDown, ArrowCircleUp, X } from "phosphor-react";
+
+
+export function NewTransactionModal() {
+  return (
+    <Dialog.Portal>
+      <Overlay />
+
+      <Content>
+        <Dialog.Title> New Transaction</Dialog.Title>
+        <CloseButton>
+          <X size={24} />
+        </CloseButton>
+
+        <form action="">
+          <input type="text" placeholder="Description" required />
+          <input type="number" placeholder="Price" required />
+          <input type="text" placeholder="Category" required />
+
+          <TransactionType>
+
+            <TransactionTypeButton variant="income">
+              <ArrowCircleUp size={24} />
+                Income
+            </TransactionTypeButton>
+
+            <TransactionTypeButton variant="outcome">
+              <ArrowCircleDown size={24} />
+                Outcome
+            </TransactionTypeButton>
+
+          </TransactionType>
+
+          <button type="submit">Register</button>
+        </form>
+
+      </Content>
+      
+    </Dialog.Portal>
+  );
+}
--- src/pages/Transactions/components/SearchForm/index.tsx
+++ src/pages/Transactions/components/SearchForm/index.tsx
@@ -5,8 +5,8 @@ import { SearchFormContainer } from "./styles";
 export function SearchForm() {
   return (
     <SearchFormContainer>
-      <input type='text' placeholder='Search by transactions' />
-      <button type='submit'>
+      <input type="text" placeholder="Search by transactions" />
+      <button type="submit">
         <MagnifyingGlass size={20} />
         Search
       </button>
