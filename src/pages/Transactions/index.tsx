8ede7831eb02209a49593dc9a176cb16dd7a8b38
Natally <natallyriqueto@hotmail.com>
2023-06-13 23:00:49 -0400

    create table for transactions

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,38 @@
 import { Header } from '../../components/Header';
 import { Summary } from '../../components/Summary';
+import { PriceHighLight, TransactionContainer, TransactionsTable } from './styles';
 
 export function Transactions() {
   return (
     <div>
       <Header />
       <Summary />
+      <TransactionContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento de site</td>
+              <td>
+                <PriceHighLight variant='income'>
+                  $12.00
+                </PriceHighLight>
+              </td>
+              <td>Venda</td>
+              <td>13/04/2021</td>
+            </tr>
+            <tr>
+              <td width="50%">Hamburguer</td>
+              <td>
+                <PriceHighLight variant='outcome'>
+                  - $59.00
+                </PriceHighLight>
+              </td>
+              <td>Alimentação</td>
+              <td>10/04/2021</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionContainer>
     </div>
   );
 }
